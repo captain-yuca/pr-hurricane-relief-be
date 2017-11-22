c4eb45f228f0416d6dc1b3731d4ca9eb23be19bd
captain-yuca <manuel.baez@upr.edu>
2017-11-22 13:32:53 -0400

    Initial commit with Spring Starter Kit

---
+++ prhurricanerelief/src/main/java/com/uprm/prhr/PrhurricanereliefApplication.java
@@ -0,0 +1,12 @@
+package com.uprm.prhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrhurricanereliefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrhurricanereliefApplication.class, args);
+	}
+}
