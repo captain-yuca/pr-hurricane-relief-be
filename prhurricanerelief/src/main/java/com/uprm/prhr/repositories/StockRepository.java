ef630786c46bf59efcd55e2438350a3b2a62ede2
Kelvin <kelvin.roche@upr.edu>
2017-12-03 17:13:12 -0400

    Added Compounding Primary Key for Stock. Fixed Some Search Lines in StockRepository.

--- prhurricanerelief/src/main/java/com/uprm/prhr/repositories/StockRepository.java
+++ prhurricanerelief/src/main/java/com/uprm/prhr/repositories/StockRepository.java
@@ -2,25 +2,32 @@ package com.uprm.prhr.repositories;
 
 import com.uprm.prhr.helpers.Region;
 import com.uprm.prhr.models.Stock;
-import com.uprm.prhr.models.StockID;
+import com.uprm.prhr.models.StockPk;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-public interface StockRepository extends CrudRepository<Stock, Long>
+public interface StockRepository extends CrudRepository<Stock, StockPk>
 {
     List<Stock> findByPricePerItem(@Param("price") Double price);
     List<Stock> findByQtySum(@Param("qty") Integer qty);
-    List<Stock> findByResourceName(@Param("resource") String resource);
-    List<Stock> findBySupplierName(@Param("name") String name);
+    @RestResource(rel="findBySupplierName", path="findBySupplierName")
+    List<Stock> findByPk_Supplier_User_Name(@Param("name") String name);
+    @RestResource(rel="findByResourceName", path="findByResourceName")
+    List<Stock> findByPk_Resource_Name(@Param("resource") String resource);
+    List<Stock> findByPkSupplierId(@Param("id") Integer id);
+    List<Stock> findByPkResourceId(@Param("id") Integer id);
 
     @RestResource(rel="findByCategory", path="findByCategory")
-    List<Stock> findByResource_Category_Name(@Param("category") String category);
-    List<Stock> findBySupplierRegion(@Param("region") Region region);
-    List<Stock> findBySupplierFName(@Param("fname") String fname);
-    List<Stock> findBySupplierLName(@Param("lname") String lname);
+    List<Stock> findByPk_Resource_Category_Name(@Param("category") String category);
+    @RestResource(rel="findBySupplierRegion", path="findBySupplierRegion")
+    List<Stock> findByPk_Supplier_User_Region(@Param("region") Region region);
+    @RestResource(rel="findBySupplierFName", path="findBySupplierFName")
+    List<Stock> findByPk_Supplier_User_FName(@Param("fname") String fname);
+    @RestResource(rel="findBySupplierLName", path="findBySupplierLName")
+    List<Stock> findByPk_Supplier_User_LName(@Param("lname") String lname);
 
 
     //TODO: Add more search queries if possible and necessary
